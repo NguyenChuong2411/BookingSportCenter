f00e129cba6daed470690ef3046f06729dd8d340
NguyenChuong2411 <nguyenchuong10072411@gmail.com>
2026-04-13 00:54:24 +0700

    Declare Full Schema in Database

---
+++ backend_api/ModelClass/BookingService/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelClass.BookingService
+{
+    [Table("reviews", Schema = "booking")]
+    public class Review
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; } // Logical FK từ Authentication
+
+        public Guid BookingId { get; set; }
+        [ForeignKey("BookingId")]
+        public Booking? Booking { get; set; }
+
+        public int Rating { get; set; }
+        public string? Comment { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
